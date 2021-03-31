8e1fc9daa5df7aaf355a6c9dd6ab019976872b86
Thomas Schuiki <463896+thomasschuiki@users.noreply.github.com>
2021-03-31 20:15:10 +0200

    add pdf generation

--- cv/generate.go
+++ cv/generate.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"text/template"
 
+	html2pdf "github.com/SebastiaanKlippert/go-wkhtmltopdf"
 	"gopkg.in/yaml.v2"
 )
 
@@ -24,6 +26,18 @@ func main() {
 }
 
 func run(args []string, stdout io.Writer) error {
+	err := generateHTML()
+	if err != nil {
+		return err
+	}
+	err = generatePDF()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func generateHTML() error {
 	out, err := os.Create("dist/index.html")
 	if err != nil {
 		return err
@@ -49,5 +63,50 @@ func run(args []string, stdout io.Writer) error {
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func generatePDF() error {
+	// Create new PDF generator
+	pdfg, err := html2pdf.NewPDFGenerator()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set global options
+	pdfg.Dpi.Set(600)
+	pdfg.Orientation.Set(html2pdf.OrientationPortrait)
+	pdfg.PageSize.Set(html2pdf.PageSizeA4)
+	pdfg.MarginTop.Set(10)
+	pdfg.MarginRight.Set(10)
+	pdfg.MarginBottom.Set(10)
+	pdfg.MarginLeft.Set(10)
+	// Create a new input page from an URL
+	page := html2pdf.NewPage("dist/index.html")
+	page.Zoom.Set(1)
+
+	// Set options for this page
+	page.FooterRight.Set("[page]")
+	page.FooterFontSize.Set(10)
+
+	page.EnableLocalFileAccess.Set(true)
+	page.DisableSmartShrinking.Set(true)
+	// Add to document
+	pdfg.AddPage(page)
+
+	// Create PDF document in internal buffer
+	err = pdfg.Create()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write buffer contents to file on disk
+	err = pdfg.WriteFile("./dist/DI Thomas Schuiki - CV.pdf")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Done")
+	// Output: Done
 	return nil
 }
--- cv/go.mod
+++ cv/go.mod
@@ -2,4 +2,7 @@ module github.com/thomasschuiki/cv
 
 go 1.16
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.6.1
+	gopkg.in/yaml.v2 v2.4.0
+)
