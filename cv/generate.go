5b25da82333a27b735c3d65cb620624549a6ada1
Thomas Schuiki <thomas.schuiki@protonmail.com>
2021-08-15 13:58:53 +0200

    remove pdf generation from go-file and add github action for it

--- cv/generate.go
+++ cv/generate.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"text/template"
 
-	html2pdf "github.com/SebastiaanKlippert/go-wkhtmltopdf"
 	"gopkg.in/yaml.v2"
 )
 
@@ -35,10 +34,6 @@ func run(args []string, stdout io.Writer) error {
 		return err
 	}
 	err = generateHTML(lang)
-	if err != nil {
-		return err
-	}
-	err = generatePDF(lang)
 	if err != nil {
 		return err
 	}
@@ -74,49 +69,3 @@ func generateHTML(lang string) error {
 	return nil
 }
 
-func generatePDF(lang string) error {
-	// Create new PDF generator
-	pdfg, err := html2pdf.NewPDFGenerator()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Set global options
-	pdfg.Dpi.Set(600)
-	pdfg.Orientation.Set(html2pdf.OrientationPortrait)
-	pdfg.PageSize.Set(html2pdf.PageSizeA4)
-	pdfg.MarginTop.Set(10)
-	pdfg.MarginRight.Set(10)
-	pdfg.MarginBottom.Set(10)
-	pdfg.MarginLeft.Set(10)
-	// Create a new input page from an URL
-	page := html2pdf.NewPage(fmt.Sprintf("dist/index.%s.html", lang))
-	page.Zoom.Set(1)
-    page.UserStyleSheet.Set("css/")
-
-	// Set options for this page
-	page.FooterRight.Set("[page]")
-	page.FooterFontSize.Set(10)
-
-	page.EnableLocalFileAccess.Set(true)
-	page.DisableSmartShrinking.Set(true)
-	// Add to document
-	pdfg.AddPage(page)
-
-	// Create PDF document in internal buffer
-	err = pdfg.Create()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Write buffer contents to file on disk
-
-	err = pdfg.WriteFile(fmt.Sprintf("dist/DI Thomas Schuiki - CV.%s.pdf", lang))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Done")
-	// Output: Done
-	return nil
-}
